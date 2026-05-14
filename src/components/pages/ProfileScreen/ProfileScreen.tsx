7dd431774db181a587949c21ef9954a2f9a338ee
Batozhab Budaev <pikapika1713@gmail.com>
2026-05-14 21:41:45 +0800

    feat: refactor ProfileScreen to use Pressable for logout and enhance layout with header styling

--- src/components/pages/ProfileScreen/ProfileScreen.tsx
+++ src/components/pages/ProfileScreen/ProfileScreen.tsx
@@ -1,21 +1,34 @@
-import * as React from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import React from 'react';
+import { Text, View, StyleSheet, Pressable } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import Ionicons from '@react-native-vector-icons/ionicons';
+
 import { signOut } from '../../../store/slices/authSlice';
 import { useAppDispatch } from '../../../store/hooks';
+import { textStyles } from '../../theme/textStyles';
 
-interface ProfileScreenProps {}
-
-const ProfileScreen = (props: ProfileScreenProps) => {
+const ProfileScreen = () => {
   const dispatch = useAppDispatch();
+  const { top } = useSafeAreaInsets();
+
   return (
-    <View style={styles.container}>
-      <Button
-        title="logout"
-        onPress={() => {
-          dispatch(signOut());
-        }}
-      />
-      <Text>MainScreen</Text>
+    <View style={[styles.container, { paddingTop: top }]}>
+      <View style={styles.header}>
+        <Text style={textStyles.text_24bold}>Profile</Text>
+        <Pressable
+          onPress={() => {
+            dispatch(signOut());
+          }}
+          style={styles.exitButton}
+        >
+          <Ionicons
+            name="log-out-outline"
+            size={30}
+            color="black"
+            style={styles.icon}
+          />
+        </Pressable>
+      </View>
     </View>
   );
 };
@@ -24,4 +37,7 @@ export default ProfileScreen;
 
 const styles = StyleSheet.create({
   container: {},
+  header: { alignItems: 'center' },
+  exitButton: { position: 'absolute', top: 0, right: 0 },
+  icon: { marginRight: 10 },
 });
